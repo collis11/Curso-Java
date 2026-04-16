f9f633e70122b250708e8053c9448c87b698347e
vitor <vitor.collis@gmail.com>
2026-04-16 16:34:39 -0300

    Herança e polimorfismo

---
+++ Polimorfismo/src/entities/SavingsAccount.java
@@ -0,0 +1,32 @@
+package entities;
+
+public  class SavingsAccount extends Account {
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRAte) {
+		super(number, holder, balance);
+		this.interestRate = interestRAte;
+	}
+
+	public Double getInterestRAte() {
+		return interestRate;
+	}
+
+	public void setInterestRAte(Double interestRAte) {
+		this.interestRate = interestRAte;
+	}
+	
+	public void updateBalance () {
+		balance += balance * interestRate;
+	}
+	
+	@Override
+	public  void withdraw (double amount) {
+		balance -= amount ;
+	}
+	
+}
