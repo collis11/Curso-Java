8cc4279c324e55905f427eb45d06102eb3bedb9f
vitor <vitor.collis@gmail.com>
2026-05-05 16:58:18 -0300

    Expressão lambda declarada

--- Consumer/src/app/Program.java
+++ Consumer/src/app/Program.java
@@ -3,9 +3,9 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUptade;
 
 public class Program {
 	public static void main (String[]args) {
@@ -18,7 +18,14 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::nonstaticPriceUptade);
+        
+        double factor = 1.1;        
+        
+        Consumer <Product> cons = p -> {
+        	p.setPrice(p.getPrice() * factor);
+        };
+        
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	}
