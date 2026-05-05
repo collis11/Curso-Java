1ebd425e3a0a05277b829f705390d3a3cd036e9c
vitor <vitor.collis@gmail.com>
2026-05-05 16:03:32 -0300

    Predicate

---
+++ Predicate/src/app/program.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+
+public class program { 
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(Product::nonstaticProductPredicate);
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
